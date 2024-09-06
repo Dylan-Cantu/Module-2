da0cd67ca8199edfcc9e070f22bd72d88c5af1cd
Dylan-Cantu <dcantu208@student.alamo.edu>
2024-09-06 14:25:02 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+var life = 100; // This is my life bar NUMBER
+
+life = life - 10;
+
+//90
+
+life = life / 10;
+
+var name = "Imed"; // This is the name displayed in the game STRING
+
+var checkout = false; //Boolean
+
+var box;
+
+console.log(life);
+
+console.log("This string of text was made using console.log") // This is my string variable
+
+var a = 10+8; // This is my global scope
+console.log(a);
+
+if (1==1) {
+    let b = "This is my block scope";
+    console.log(b)
+} // This is my block scope
+
+
+
+
